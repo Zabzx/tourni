b01aa57cfb5d522f556b5bc8608ea89bece012a5
Ziabeher Phillips <zabq02@gmail.com>
2022-02-21 21:51:35 -0400

    Animation fixes

--- main.js
+++ main.js
@@ -7,14 +7,30 @@ tl.fromTo('[data-main-hero]', {opacity: 0, y: -50}, {opacity: 1, y: 0});
 tl.fromTo('[data-sub-hero]', {opacity: 0, x: -100}, {opacity: 1, x: 0}, '<');
 
 //Divider Animations
-const dtl = gsap.timeline({defaults: {duration: 1, ease: "back.out"}, scrollTrigger: {
-    trigger: ".community-text",
+
+
+
+
+const ctl = gsap.timeline({defaults: {duration: 1, ease: "back.out"}, scrollTrigger: {
+    trigger: '.community-text',
+    toggleActions: 'restart none none none'
+}})
+
+ctl.fromTo('.community-text', {opacity: 0, x: -200}, {opacity: 1, x: 0})
+ctl.fromTo('.community-icon', {opacity: 0, y: -200}, {opacity: 1, y: 0}, '<')
+
+const etl = gsap.timeline({defaults: {duration: 1, ease: "back.out"}, scrollTrigger: {
+    trigger: '.events',
     toggleActions: 'restart none none none'
 }});
 
-dtl.fromTo('.community-text', {opacity: 0, x: -200}, {opacity: 1, x: 0});
-dtl.fromTo('.community-icon', {opacity: 0, y: -200}, {opacity: 1, y: 0}, '<')
-dtl.fromTo('.e-icon', {opacity: 0, x: 200}, {opacity: 1, x: 0})
-dtl.fromTo('.e-text', {opacity: 0, y: -200}, {opacity: 1, y: 0}, '<')
-dtl.fromTo('.g-text', {opacity: 0, x: -200}, {opacity: 1, x: 0});
-dtl.fromTo('.g-icon', {opacity: 0, y: -200}, {opacity: 1, y: 0})
+etl.fromTo('.e-icon', {opacity: 0, x: 200}, {opacity: 1, x: 0});
+etl.fromTo('.e-text', {opacity: 0, y: -200}, {opacity: 1, y: 0});
+
+const gtl = gsap.timeline({defaults: {duration: 1, ease: "back.out"}, scrollTrigger: {
+    trigger: '.giveaways',
+    toggleActions: 'restart none none none'
+}});
+
+gtl.fromTo('.g-text', {opacity: 0, x: -200, duration: 0.25}, {opacity: 1, x: 0});
+gtl.fromTo('.g-icon', {opacity: 0, y: -200,}, {opacity: 1, y: 0});
